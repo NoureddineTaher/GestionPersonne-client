a1937fb93cbddb073f7f109a4450952d1d41e90f
nouri <noureddine_taher@yahoo.com>
2021-10-14 16:50:50 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmployeeService } from './service/employee.service';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
+import { DataTablesModule } from 'angular-datatables';
+import { FooterComponent } from './employee/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeListComponent,
+    UpdateEmployeeComponent,
+    FooterComponent,
+    
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    DataTablesModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
